ae550bdac06391211c1038c249b05a65af8d4819
Kun Woo <wkw8402@berkeley.edu>
2022-12-14 16:44:18 -0800

    Initial Commit

---
+++ Viers/ViersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViersApp.swift
+//  Viers
+//
+//  Created by Kun  on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ViersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
